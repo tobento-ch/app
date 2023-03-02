182f1a16b305493195ad5b5118ccd5282b733f39
TOBENTOch <info@tobento.ch>
2023-03-02 14:02:49 +0100

    namespace added and functions added

--- src/functions.php
+++ src/functions.php
@@ -15,8 +15,10 @@ namespace Tobento\App;
 
 use Psr\Container\ContainerInterface;
 use Tobento\Service\HelperFunction\Functions;
+use Tobento\Service\Config\ConfigInterface;
+use Tobento\Service\Config\ConfigNotFoundException;
 
-if (!function_exists('app')) {
+if (!function_exists(__NAMESPACE__.'\app')) {
     /**
      * Returns the app.
      *
@@ -25,5 +27,37 @@ if (!function_exists('app')) {
     function app(): AppInterface
     {
         return Functions::get(ContainerInterface::class)->get(AppInterface::class);
+    }
+}
+
+if (!function_exists(__NAMESPACE__.'\directory')) {
+    /**
+     * Returns the specified directory.
+     *
+     * @param string $name
+     * @return string
+     */
+    function directory(string $name): string
+    {
+        return app()->dir($name);
+    }
+}
+
+if (!function_exists(__NAMESPACE__.'\config')) {
+    /**
+     * Returns a config value by key.
+     *
+     * @param string $key The key.
+     * @param mixed $default A default value.
+     * @param null|int|string|array $locale 
+     *        string: locale,
+     *        array: [] if empty gets all languages,
+     *        otherwise the keys set ['de', 'en']
+     * @return mixed The value or the default value if not exist.
+     * throws ConfigNotFoundException
+     */
+    function config(string $key, mixed $default = null, null|int|string|array $locale = null): mixed
+    {
+        return app()->get(ConfigInterface::class)->get($key, $default, $locale);
     }
 }
